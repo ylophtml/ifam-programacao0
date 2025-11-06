4a896e73f3603f77ecbada7cd9c5d635e36188a8
ylophtml <mpoliana328@gmail.com>
2025-11-06 01:27:46 -0400

    Update vetor7.js

--- vetor7.js
+++ vetor7.js
@@ -1,8 +1,12 @@
 let n = parseInt(prompt("Quantos números deseja somar?"))
-const numeros = []
+let numeros = []
 let soma = 0
-for(let i = 0; i<n; i++){
-if (soma > 100)
+for (let i = 0; i < n; i++) {
+  let numero = parseInt(prompt("Digite um número"))
+  numeros[i] = numero
+  soma = soma + numero
+}
+if (soma > 100) {
   document.writeln("A soma é " + soma)
 } else {
   document.writeln("A soma é menor ou igual a 100")
