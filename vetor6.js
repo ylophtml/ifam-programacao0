3e85b8aed190558afa50d33750db82bba98053f1
ylophtml <mpoliana328@gmail.com>
2025-11-06 01:38:14 -0400

    Add item price input and total calculation

---
+++ vetor6.js
@@ -0,0 +1,13 @@
+let n = parseInt(prompt("Quanto?"));
+let preco = [];
+let i = 0;
+let total = 0;
+
+while (i < n) {
+  preco[i] = parseFloat(prompt("Preço do item " + (i + 1) + ":"));
+  total = total + preco[i];
+  i = i + 1;
+}
+
+document.writeln("Quantidade de itens: " + n + "<br>");
+document.writeln("Valor total: R$ " + total.toFixed(2));
