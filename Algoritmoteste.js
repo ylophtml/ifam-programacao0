da7390aa0d3fed8349f7986b98c13dfb1fb7bdae
ylophtml <mpoliana328@gmail.com>
2025-11-10 18:50:27 -0400

    Add presentation feedback based on user input

---
+++ Algoritmoteste.js
@@ -0,0 +1,8 @@
+let apresentacao = prompt("qual vai ser a forma de apresentar?")
+if(apresentacao === `algoritmo`){
+alert("espetáculo")
+}else if(apresentacao === `slide`){
+alert("bom")
+}else{
+alert("fracasso na apresentação")
+}
